9e26455fdac975c7370e2154dacad31c4d3afab5
Alexandr <alesher696@gmail.com>
2023-09-08 16:05:33 +0300

    selectors, dialogsList

--- src/common/components/users/Users.tsx
+++ src/common/components/users/Users.tsx
@@ -37,9 +37,6 @@ export const Users = () => {
 
     useEffect(() => {
         dispatch(getUsersTC(currentPage, pageSize))
-    }, [])
-
-    useEffect(() => {
         dispatch(getFollowedUsersTC())
     }, [])
 
